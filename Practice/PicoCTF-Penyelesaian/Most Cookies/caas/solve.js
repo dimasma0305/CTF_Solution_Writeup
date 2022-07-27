ae2abfa3ec6b1549edef8285bd864aeb5434c4d8
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-28 06:00:01 +0800

    Wed Jul 27 10:00:01 PM UTC 2022

--- Practice/PicoCTF-Penyelesaian/Most Cookies/caas/solve.js
+++ Practice/PicoCTF-Penyelesaian/Most Cookies/caas/solve.js
@@ -1,21 +1,23 @@
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
-  });
-const url = "https://caas.mars.picoctf.net";
+});
 
 const get_cmd = (comand) => {
-    fetch(url+"/cowsay/abc;"+comand).then(text => text.text())
-    .then(text => {
-        console.log(text.substring(145, text.length-1));
-    })
+    fetch(url + "/cowsay/abc;" + comand).then(text => text.text())
+        .then(text => {
+            console.log(text.substring(145, text.length - 1));
+        })
+    return null;
 }
 
-while (true) {
+const url = "https://caas.mars.picoctf.net";
+
+
+(function loop() {
     readline.question("Enter command: ", (cmd) => {
         cmd = encodeURIComponent(cmd);
         get_cmd(cmd);
         readline.close();
     });
-}
-
+}());
